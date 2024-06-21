ce7e3460e8384ac3caad51d40cf6d3cab85ff655
nidakhan990 <164150254+nidakhan990@users.noreply.github.com>
2024-06-21 20:52:08 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -5,7 +5,7 @@
 terraform {
   backend "s3" {
     bucket = "my-buck-88"
-    key    = "Module/Open_Vpn/terraform.tfstate"
+    key    = "Module/open_Vpn/terraform.tfstate"
     region = "us-east-2"
     //dynamodb_table = "my_tool_dynamo"
   }
